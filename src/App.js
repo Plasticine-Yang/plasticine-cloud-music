7eaf0c00b7dd957e89d38681c9b0e5e21fc419b1
plasticine <975036719@qq.com>
2022-10-25 18:58:45 +0800

    feat: 集成redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 
 import { IconStyle } from './assets/iconfont/iconfont'
+import { GlobalStyle } from './style'
 
 import router from './router'
 
-import { GlobalStyle } from './style'
+import store from './store'
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
       <RouterProvider router={router} />
-    </>
+    </Provider>
   )
 }
 
