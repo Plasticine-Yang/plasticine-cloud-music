13272793dce2e9219d1e18a277ace05c3904f55e
plasticine <975036719@qq.com>
2022-10-25 20:08:27 +0800

    feat: swiper component

--- src/application/Recommend/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Recommend() {
-  return <div>Recommend</div>
-}
-
-export default React.memo(Recommend)
---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+
+import Swiper, { Pagination } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+import { SliderContainer } from './style'
+
+function Slider(props) {
+  const [sliderSwiper, setSliderSwiper] = useState(null)
+  const { bannerList } = props
+
+  useEffect(() => {
+    // 初始化 swiper 实例
+    if (bannerList.length && !sliderSwiper) {
+      let newSliderSwiper = new Swiper('.slider-container', {
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        modules: [Pagination],
+        pagination: { el: '.swiper-pagination' },
+      })
+      setSliderSwiper(newSliderSwiper)
+    }
+  }, [bannerList.length, sliderSwiper])
+
+  return (
+    <SliderContainer>
+      <div className="before"></div>
+      <div className="slider-container">
+        <div className="swiper-wrapper">
+          {bannerList.map(slider => (
+            <div className="swiper-slide" key={slider.id}>
+              <div className="slider-nav">
+                <img
+                  src={slider.imageUrl}
+                  width="100%"
+                  height="100%"
+                  alt="推荐"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="swiper-pagination"></div>
+      </div>
+    </SliderContainer>
+  )
+}
+
+export default React.memo(Slider)
